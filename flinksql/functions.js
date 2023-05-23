b13300597f50f7c049c2c1b7eb553910637ffa6e
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2023-05-23 14:15:17 -0400

    Create functions.js

---
+++ flinksql/functions.js
@@ -0,0 +1,27 @@
+function DISTANCE_BETWEEN(lat1, lon1, lat2, lon2) {
+  function toRad(x) {
+    return x * Math.PI / 180;
+  }
+
+  // https://community.cloudera.com/t5/Community-Articles/Flink-SSB-Credit-Card-Fraud-Detection-Demo/ta-p/334792?es_id=55b2d12b4d
+  lat1 = parseFloat(lat1);
+  lon1 = parseFloat(lon1);
+  lat2 = parseFloat(lat2);
+  lon2 = parseFloat(lon2);
+  
+  var R = 6371; // km
+  
+  var x1 = lat2 - lat1;
+  var dLat = toRad(x1);
+  var x2 = lon2 - lon1;
+  var dLon = toRad(x2)
+  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2); 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  
+  // convert to miles
+  return (d / 1.60934);
+}
+DISTANCE_BETWEEN($p0, $p1, $p2, $p3)
